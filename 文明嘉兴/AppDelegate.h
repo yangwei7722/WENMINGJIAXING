66293fa6b819124cd2633a35f319a0e8600521fb
杨伟 <yangwei@yangweidemacbook-air.local>
2016-06-04 12:34:24 +0800

    Initial Commit

---
+++ 文明嘉兴/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  文明嘉兴
+//
+//  Created by yangwei on 16/6/4.
+//  Copyright © 2016年 yangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 文明嘉兴/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  文明嘉兴
+//
+//  Created by yangwei on 16/6/4.
+//  Copyright © 2016年 yangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
